04633f87128606f7acfe87db2314332493a8f137
xmojzi09 <xmojzi09@vutbr.cz>
2021-04-19 11:02:42 +0200

    'uploaded'

--- searching.py
+++ searching.py
@@ -20,11 +20,35 @@ def read_data(file_name, field):
     else:
         return file[field]
 
+def linear_search(sequence, number=5):
+    indices = []
+    count = 0
+    i = 0
+
+    for i in range (0,len(sequence),1):
+        if sequence[i] == number:
+            indices.append(i)
+            count += 1
+    return {'positions': indices,'count': count}
+
+def pattern_search(sequence, pattern):
+    l = len(pattern)
+    indices = []
+
+    for i in range(0,(len(sequence)-3),1):
+        if sequence[i:i+l] == pattern:
+            indices.append(i)
+    return indices
 
 def main():
-    file = read_data('sequential.json', 'dna_sequence')
+    file = read_data('sequential.json', 'unordered_numbers')
     print(file)
-
+    number = int(input('Zadajte cislo: '))
+    results = linear_search(file, number)
+    print(results)
+    seq = input('Zadajte hladanu sekvenciu:')
+    pattern = pattern_search(read_data('sequential.json', 'dna_sequence'), seq)
+    print(pattern)
 
 if __name__ == '__main__':
     main()
